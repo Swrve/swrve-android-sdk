bf7e22f171ad623906aa8ac02884efc6b71c4a91
Sergio-Mira <sergio.mira@swrve.com>
2014-10-23 15:46:47 +0100

    Added internal comment to new classes

--- SwrveCommonSDK/src/main/java/com/swrve/sdk/device/ITelephonyManager.java
+++ SwrveCommonSDK/src/main/java/com/swrve/sdk/device/ITelephonyManager.java
@@ -1,5 +1,8 @@
 package com.swrve.sdk.device;
 
+/**
+ * Used internally to obtain carrier information.
+ */
 public interface ITelephonyManager {
     String getSimOperatorName();
     String getSimCountryIso();
