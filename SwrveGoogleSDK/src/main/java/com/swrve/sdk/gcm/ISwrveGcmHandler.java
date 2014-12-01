e8af7c74cb368f8b2a5bd9b5fd1607dc9003b080
dominicmarmionswrve <dominic.marmion@swrve.com>
2014-12-01 09:43:05 +0000

    Added interfaces to concrete classes to make them more testable

---
+++ SwrveGoogleSDK/src/main/java/com/swrve/sdk/gcm/ISwrveGcmHandler.java
@@ -0,0 +1,28 @@
+package com.swrve.sdk.gcm;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+interface ISwrveGcmHandler {
+    boolean onHandleIntent(Intent intent, GoogleCloudMessaging gcm);
+
+    void processNotification(Bundle msg);
+
+    boolean mustShowNotification();
+
+    int showNotification(NotificationManager notificationManager, Notification notification);
+
+    NotificationCompat.Builder createNotificationBuilder(String msgText, Bundle msg);
+
+    Notification createNotification(Bundle msg, PendingIntent contentIntent);
+
+    PendingIntent createPendingIntent(Bundle msg);
+
+    Intent createIntent(Bundle msg);
+}
