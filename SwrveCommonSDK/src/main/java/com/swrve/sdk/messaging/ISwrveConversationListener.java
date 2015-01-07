f9298af268ec68796c8608a18c9c49b6617dd312
Shane Moore <shane@converser.io>
2015-01-07 10:50:05 +0000

    Initial commit. Behaves the same as ISwrveMessageListener except it is used for conversations.

---
+++ SwrveCommonSDK/src/main/java/com/swrve/sdk/messaging/ISwrveConversationListener.java
@@ -0,0 +1,17 @@
+package com.swrve.sdk.messaging;
+
+/**
+ * Implement this interface to handle the rendering of in-app conversations
+ * completely from your app. You will have to render and manage these
+ * conversations yourself.
+ */
+public interface ISwrveConversationListener {
+    /**
+     * This method is invoked when a conversation should be shown in your app.
+     *
+     * @param conversation   coonversation to be shown.
+     * @param firstTime indicates if this message was already showing and the app
+     *                  rotated.
+     */
+    void onMessage(SwrveConversation conversation, boolean firstTime);
+}
