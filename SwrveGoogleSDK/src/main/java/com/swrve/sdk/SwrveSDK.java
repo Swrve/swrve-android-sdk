0b4eada4c2095162dd3d8d8cedac4ba44042c6b6
dominicmarmionswrve <dominic.marmion@swrve.com>
2015-01-26 11:09:35 +0000

    moved javadoc comments from ISwrve to SwrveSDK becuase this is now the public facing api.

--- SwrveGoogleSDK/src/main/java/com/swrve/sdk/SwrveSDK.java
+++ SwrveGoogleSDK/src/main/java/com/swrve/sdk/SwrveSDK.java
@@ -9,8 +9,6 @@ import com.swrve.sdk.gcm.ISwrvePushNotificationListener;
 
 public class SwrveSDK extends SwrveSDKBase {
 
-    // TODO DOM review all javadoc comments here
-
     /**
      * Create a single Swrve SDK instance.
      * @param context your activity or application context
@@ -46,26 +44,79 @@ public class SwrveSDK extends SwrveSDKBase {
         return (ISwrve) instance;
     }
 
+    /**
+     * Returns the Swrve configuration that was used to initialize the SDK.
+     *
+     * @return configuration used to context the SDK
+     */
     public static SwrveConfig getConfig() {
         checkInstanceCreated();
         return (SwrveConfig) instance.getConfig();
     }
 
+    /**
+     * Set the push notification listener. It is only called if the activity's intent is processed.
+     *
+     * @param pushNotificationListener
+     */
     public static void setPushNotificationListener(ISwrvePushNotificationListener pushNotificationListener) {
         checkInstanceCreated();
         ((ISwrve) instance).setPushNotificationListener(pushNotificationListener);
     }
 
+    /**
+     * Add a Swrve.iap event to the event queue. This event should be added
+     * for unvalidated real money transactions in the Google Play Store, where a single item was purchased.
+     * (i.e where no in-app currency or bundle was purchased)
+     *
+     * @param productId     Unique product identifier for the item bought. This should
+     *                      match the Swrve resource name.
+     *                      Required, cannot be empty.
+     * @param productPrice  The price (in real money) of the product that was purchased.
+     *                      Note: this is not the price of the total transaction, but the per-product price.
+     *                      Must be greater than or equal to zero.
+     * @param currency      real world currency used for this transaction. This must be an ISO
+     *                      currency code. A typical value would be "USD". Required, cannot be empty.
+     * @param dataSignature The purchase data received from Google Play. Required, cannot be empty.
+     * @param dataSignature The signature for the purchase data. Required, cannot be empty.
+     */
     public static void iapPlay(String productId, double productPrice, String currency, String purchaseData, String dataSignature) {
         checkInstanceCreated();
         ((ISwrve) instance).iapPlay(productId, productPrice, currency, purchaseData, dataSignature);
     }
 
+    /**
+     * Add a Swrve.iap event to the event queue. This event should be added for unvalidated real
+     * money transactions in the Google Play Store, where in-app currency was purchased
+     * or where multiple items and/or currencies were purchased.
+     * <p/>
+     * To create the rewards object, create an instance of SwrveIAPRewards and
+     * use addItem() and addCurrency() to add the individual rewards
+     *
+     * @param productId     Unique product identifier for the item bought. This should match the
+     *                      Swrve resource name. Required, cannot be empty.
+     * @param productPrice  price of the product in real money. Note that this is the price
+     *                      per product, not the total price of the transaction (when quantity > 1)
+     *                      A typical value would be 0.99. Must be greater or equal to zero.
+     * @param currency      real world currency used for this transaction. This must be an
+     *                      ISO currency code. A typical value would be "USD".
+     *                      Required, cannot be empty.
+     * @param rewards       SwrveIAPRewards object containing any in-app currency and/or additional
+     *                      items included in this purchase that need to be recorded.
+     * @param dataSignature The purchase data received from Google Play. Required, cannot be empty.
+     * @param dataSignature The signature for the purchase data. Required, cannot be empty.
+     */
     public static void iapPlay(String productId, double productPrice, String currency, SwrveIAPRewards rewards, String purchaseData, String dataSignature) {
         checkInstanceCreated();
         ((ISwrve) instance).iapPlay(productId, productPrice, currency, rewards, purchaseData, dataSignature);
     }
 
+    /**
+     * Process the push notification received from GCM that opened the app. This should be called on
+     * the Activity's onCreate that was opened from a push notification.
+     *
+     * @param intent The intent that opened the activity
+     */
     public static void processIntent(Intent intent) {
         checkInstanceCreated();
         ((ISwrve) instance).processIntent(intent);
