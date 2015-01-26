ca13b2f545bef35fb9f87260b0aa52885bd13298
Sergio-Mira <sergio.mira@swrve.com>
2015-01-26 16:55:39 +0000

    Removed code duplication, serializable conversations

--- SwrveCommonSDK/src/main/java/com/swrve/sdk/converser/engine/model/MultiValueLongInput.java
+++ SwrveCommonSDK/src/main/java/com/swrve/sdk/converser/engine/model/MultiValueLongInput.java
@@ -1,15 +1,16 @@
 package com.swrve.sdk.converser.engine.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class MultiValueLongInput extends InputBase {
+public class MultiValueLongInput extends InputBase implements Serializable {
     private ArrayList<Item> values;
 
     public ArrayList<Item> getValues() {
         return values;
     }
 
-    public static class Item {
+    public static class Item  implements Serializable {
         private String title;
         private ArrayList<String> options;
 
