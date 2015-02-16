176dbc4ed74479e9039eebbbe6746325780511a9
Shane Moore <shane@converser.io>
2015-02-16 15:01:42 +0000

    Fixed issues which were stopping the deep links from being launched - Intents not being launched properly with activity flags - Control Actions were not able to be serialized correctly

--- SwrveCommonSDK/src/main/java/com/swrve/sdk/converser/engine/model/ControlActions.java
+++ SwrveCommonSDK/src/main/java/com/swrve/sdk/converser/engine/model/ControlActions.java
@@ -2,9 +2,10 @@ package com.swrve.sdk.converser.engine.model;
 
 import android.net.Uri;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class ControlActions {
+public class ControlActions implements Serializable {
     public static final Object CALL_ACTION = "call";
     public static final Object VISIT_URL_ACTION = "visit";
     public static final Object DEEPLINK_ACTION = "deeplink";
