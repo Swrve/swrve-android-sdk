4c04b4fee2512696aff2f079821b1d0d656e813a
Sergio-Mira <sergio.mira@swrve.com>
2015-03-25 15:44:05 +0000

    Point demo to production

--- SwrveSDKDemo/src/com/swrve/sdk/demo/DemoApplication.java
+++ SwrveSDKDemo/src/com/swrve/sdk/demo/DemoApplication.java
@@ -20,11 +20,7 @@ public class DemoApplication extends Application {
         super.onCreate();
 
         try {
-            SwrveConfig config = new SwrveConfig();
-            // TODO: Converser setup, remove
-            config.setEventsUrl(new URL("http://converser-api.swrve.com"));
-            config.setContentUrl(new URL("http://converser-content.swrve.com"));
-            SwrveSDK.createInstance(this, YOUR_APP_ID, YOUR_API_KEY, config); // TODO DOM rake build script looks for appid/appkey in MainActivity. Change this.
+            SwrveSDK.createInstance(this, YOUR_APP_ID, YOUR_API_KEY); // TODO DOM rake build script looks for appid/appkey in MainActivity. Change this.
         } catch (IllegalArgumentException exp) {
             Log.e(LOG_TAG, "Could not initialize the Swrve SDK", exp);
         } catch (MalformedURLException e) {
