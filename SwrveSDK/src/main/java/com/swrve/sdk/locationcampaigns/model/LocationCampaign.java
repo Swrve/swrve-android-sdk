bc90404c165bdee3a05d418fdb88be2d2d463881
dominicmarmionswrve <dominic.marmion@swrve.com>
2015-09-14 17:19:02 +0100

    9551-Android_Location_Campaign_Match

--- SwrveSDK/src/main/java/com/swrve/sdk/locationcampaigns/model/LocationCampaign.java
+++ SwrveSDK/src/main/java/com/swrve/sdk/locationcampaigns/model/LocationCampaign.java
@@ -1,5 +1,7 @@
 package com.swrve.sdk.locationcampaigns.model;
 
+import android.support.annotation.VisibleForTesting;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -12,6 +14,15 @@ public class LocationCampaign  {
     private long end;
     private int version;
     private LocationMessage message;
+
+    @VisibleForTesting
+    public LocationCampaign(int id, long start, long end, int version, LocationMessage message) {
+        this.id = id;
+        this.start = start;
+        this.end = end;
+        this.version = version;
+        this.message = message;
+    }
 
     public int getId() {
         return id;
@@ -37,5 +48,16 @@ public class LocationCampaign  {
         Gson gson = new Gson();
         Map<String, LocationCampaign> locationCampaigns = gson.fromJson(jsonString, new TypeToken<Map<String, LocationCampaign>>(){}.getType());
         return locationCampaigns;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationCampaign{" +
+                "id=" + id +
+                ", start=" + start +
+                ", end=" + end +
+                ", version=" + version +
+                ", message=" + message +
+                '}';
     }
 }
