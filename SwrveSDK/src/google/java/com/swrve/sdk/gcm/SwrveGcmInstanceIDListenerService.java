57880e83fd0deafa2377561ccf0ac3fa724ba575
Sergio-Mira <sergio.mira@swrve.com>
2015-10-20 18:11:59 +0100

    Ported fix from 4.0.4

--- SwrveSDK/src/google/java/com/swrve/sdk/gcm/SwrveGcmInstanceIDListenerService.java
+++ SwrveSDK/src/google/java/com/swrve/sdk/gcm/SwrveGcmInstanceIDListenerService.java
@@ -1,7 +1,9 @@
 package com.swrve.sdk.gcm;
 
-import com.swrve.sdk.SwrveLogger;
+import android.util.Log;
 import com.google.android.gms.iid.InstanceIDListenerService;
+import com.swrve.sdk.ISwrveBase;
+import com.swrve.sdk.Swrve;
 import com.swrve.sdk.SwrveSDK;
 
 public class SwrveGcmInstanceIDListenerService extends InstanceIDListenerService {
@@ -10,11 +12,11 @@ public class SwrveGcmInstanceIDListenerService extends InstanceIDListenerService
 
     @Override
     public void onTokenRefresh() {
-        SwrveSDK sdk = (SwrveSDK)SwrveSDK.getInstance();
-        if (sdk != null) {
-            sdk.onTokenRefreshed();
+        ISwrveBase sdk = SwrveSDK.getInstance();
+        if (sdk != null && sdk instanceof Swrve) {
+            ((Swrve)sdk).onTokenRefreshed();
         } else {
-            SwrveLogger.e(TAG, "Could not notify the SDK of a new token. Consider using the shared instance.");
+            Log.e(TAG, "Could not notify the SDK of a new token. Consider using the shared instance.");
         }
     }
 }
