da4819eddf6b7509d38fe905233484de1be2505d
dominicmarmionswrve <dominic.marmion@swrve.com>
2015-11-05 17:34:50 +0000

    10358-remove_location_dates_from_android_sdk

--- SwrveSDK/src/main/java/com/swrve/sdk/locationcampaigns/model/LocationCampaign.java
+++ SwrveSDK/src/main/java/com/swrve/sdk/locationcampaigns/model/LocationCampaign.java
@@ -10,30 +10,18 @@ import java.util.Map;
 public class LocationCampaign  {
 
     private String id;
-    private final long start;
-    private final long end;
     private final int version;
     private final LocationMessage message;
 
     @VisibleForTesting
-    public LocationCampaign(String id, long start, long end, int version, LocationMessage message) {
+    public LocationCampaign(String id, int version, LocationMessage message) {
         this.id = id;
-        this.start = start;
-        this.end = end;
         this.version = version;
         this.message = message;
     }
 
     public String getId() {
         return id;
-    }
-
-    public long getStart() {
-        return start;
-    }
-
-    public long getEnd() {
-        return end;
     }
 
     public int getVersion() {
@@ -58,8 +46,6 @@ public class LocationCampaign  {
     public String toString() {
         return "LocationCampaign{" +
                 "id=" + id +
-                ", start=" + start +
-                ", end=" + end +
                 ", version=" + version +
                 ", message=" + message +
                 '}';
