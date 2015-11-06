6377a13ff57e4cd1c4543ff247b149bd4716d156
Shane Moore <shane.moore@swrve.com>
2015-11-06 12:14:02 +0000

    Rolled back bad commit

--- SwrveSDKDemo/src/com/swrve/sdk/demo/DemoApplication.java
+++ SwrveSDKDemo/src/com/swrve/sdk/demo/DemoApplication.java
@@ -4,11 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.swrve.sdk.SwrveSDK;
-import com.swrve.sdk.config.SwrveConfig;
-import com.swrve.sdk.config.SwrveConfigBase;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class DemoApplication extends Application {
 
@@ -18,23 +13,8 @@ public class DemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        SwrveConfig config = new SwrveConfig();
-
         try {
-            config.useEuStack();
-            config.setStack(SwrveConfigBase.SwrveStack.US);
-            config.setAppIdPrefix(2132);
-            config.setUseHttpsForEventsUrl(true);
-            config.setEventsUrl(new URL("https://featurestack12-api.swrve.com")); // Calling these directly should lock them
-            config.setContentUrl(new URL("https://featurestack12-content.swrve.com"));
-            config.setUseHttpsForEventsUrl(false); // Should still be http
-            config.setStack(SwrveConfigBase.SwrveStack.US); // Should still be featurestack 12
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            SwrveSDK.createInstance(this, 2132, "AYgy6V2uk6f3d46ShfJ", config);
+            SwrveSDK.createInstance(this, YOUR_APP_ID, YOUR_API_KEY);
         } catch (IllegalArgumentException exp) {
             Log.e(LOG_TAG, "Could not initialize the Swrve SDK", exp);
         }
