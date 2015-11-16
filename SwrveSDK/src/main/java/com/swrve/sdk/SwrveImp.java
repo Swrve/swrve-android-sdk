69d1ba901b2ee67423cdde952dcd8551b34f232a
Sergio-Mira <sergio.mira@swrve.com>
2015-11-16 14:11:52 +0000

    Started Android 4.2

--- SwrveSDK/src/main/java/com/swrve/sdk/SwrveImp.java
+++ SwrveSDK/src/main/java/com/swrve/sdk/SwrveImp.java
@@ -94,7 +94,7 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 abstract class SwrveImp<T, C extends SwrveConfigBase> {
     protected static final String PLATFORM = "Android ";
-    protected static String version = "4.1";
+    protected static String version = "4.2";
     protected static final String CAMPAIGN_CATEGORY = "CMCC2"; // Saved securely
     protected static final String LOCATION_CAMPAIGN_CATEGORY = "LocationCampaign";
     protected static final String CAMPAIGN_SETTINGS_CATEGORY = "SwrveCampaignSettings";
