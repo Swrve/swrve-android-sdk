25b4bf6a3bf9879300a211f37979395a954c7928
Dominic Marmion <dominicmarmion@ip-192-168-1-158.ec2.internal>
2016-08-15 11:33:17 +0100

    [android] 13669_fix_flaky_SwrveInAppMessageActivityTest

--- SwrveSDKTest/src/test/java/com/swrve/sdk/SwrveBaseTest.java
+++ SwrveSDKTest/src/test/java/com/swrve/sdk/SwrveBaseTest.java
@@ -12,7 +12,9 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowLog;
 
 @RunWith(RobolectricGradleTestRunner.class)
@@ -21,11 +23,13 @@ import org.robolectric.shadows.ShadowLog;
 public abstract class SwrveBaseTest {
 
     protected Activity mActivity;
+    protected ShadowActivity mShadowActivity;
 
     @Before
     public void setUp() throws Exception {
-        mActivity = Robolectric.buildActivity(MainActivity.class).create().visible().get();
         ShadowLog.stream = System.out;
+        mActivity = Robolectric.buildActivity(MainActivity.class).create().visible().get();
+        mShadowActivity = Shadows.shadowOf(mActivity);
     }
 
     @After
