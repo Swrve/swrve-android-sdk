8ed348636549636a74373b8ffa97cdaba2ecc79b
Sergio-Mira <sergio.mira@swrve.com>
2016-08-19 09:48:09 +0100

    Added a test to make sure manual registration id is sent correctly

--- SwrveSDKTest/src/testGoogle/java/com/swrve/sdk/SwrveGoogleUnitTest.java
+++ SwrveSDKTest/src/testGoogle/java/com/swrve/sdk/SwrveGoogleUnitTest.java
@@ -10,7 +10,11 @@ import com.swrve.sdk.config.SwrveConfig;
 import com.swrve.sdk.gcm.SwrveGcmConstants;
 import com.swrve.sdk.test.BuildConfig;
 import com.swrve.sdk.test.MainActivity;
+import com.swrve.sdk.utils.TestHttpServer;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +27,9 @@ import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowLog;
 
 import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -31,7 +38,7 @@ import static org.junit.Assert.assertTrue;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-public class SwrveGoogleUnitTest {
+public class SwrveGoogleUnitTest extends SwrveBaseTest {
 
     private Activity activity;
 
@@ -79,14 +86,43 @@ public class SwrveGoogleUnitTest {
     }
 
     @Test
-    public void testManualRegistrationId() {
+    public void testManualRegistrationId() throws InterruptedException, JSONException {
         SwrveConfig config = new SwrveConfig();
+        initLocalHttpServer(activity, config);
         config.setSenderId("12345");
         config.setObtainRegistrationIdEnabled(false);
         Swrve swrve = (Swrve) SwrveSDK.createInstance(activity, 1, "apiKey", config);
+        swrve.onCreate(activity);
         swrve.setRegistrationId("manual_reg_id");
 
-        // An event must be sent
-        // TOOD: HTTP mock server check that an update event was sent with the right property
+        // Find a POST with device info where we sent the right new sender ID
+        boolean foundPost = false;
+        int retries = 10;
+        while(!foundPost && (retries--) > 0) {
+            Iterator<TestHttpServer.TestHttpPostRequest> it = httpEventServer.postRequests.iterator();
+            while(it.hasNext()) {
+                TestHttpServer.TestHttpPostRequest request = it.next();
+                if (request.body.contains("swrve.gcm_token")) {
+                    JSONArray jsonEvents = request.getEventsJSON();
+                    if (jsonEvents != null && jsonEvents.length() > 0) {
+                        for (int i = 0; i < jsonEvents.length() && !foundPost; i++) {
+                            JSONObject jsonEvent = jsonEvents.getJSONObject(i);
+                            if (jsonEvent.get("type").equals("user")) {
+                                JSONObject jsonAttributes = jsonEvent.getJSONObject("attributes");
+                                if (jsonAttributes.has("swrve.gcm_token")) {
+                                    foundPost = jsonAttributes.get("swrve.gcm_token").equals("manual_reg_id");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!foundPost) {
+                Thread.sleep(1000);
+            }
+        }
+        assertTrue(foundPost);
+        swrve.onDestroy(activity);
     }
 }
